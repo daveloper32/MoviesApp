533d1b1ff576ec6dabf4f34ed4d928eb2902333b
daveloper32 <80218906+daveloper32@users.noreply.github.com>
2021-12-22 02:12:03 -0500

    Adding Splash Screen

--- app/src/main/java/com/daveloper/moviesapp/ui/view/MainActivity.kt
+++ app/src/main/java/com/daveloper/moviesapp/ui/view/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.daveloper.moviesapp.ui.view
 
+import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.daveloper.moviesapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_MoviesApp)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
