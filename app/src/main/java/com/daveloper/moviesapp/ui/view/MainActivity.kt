a1f000c5c342d54e50fb8a2c1fbee48cf006638d
daveloper32 <80218906+daveloper32@users.noreply.github.com>
2021-12-28 22:00:56 -0500

    Setting Bottom Navigation View (2 slides [main & search])

--- app/src/main/java/com/daveloper/moviesapp/ui/view/MainActivity.kt
+++ app/src/main/java/com/daveloper/moviesapp/ui/view/MainActivity.kt
@@ -1,16 +1,63 @@
 package com.daveloper.moviesapp.ui.view
 
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.daveloper.moviesapp.R
+import com.daveloper.moviesapp.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationBarView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity()
+{
+    // Init Vars
+    // Binding
+    private lateinit var binding: ActivityMainBinding
+    //
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         //After Splash style, setting the Theme to Theme_MoviesApp
         setTheme(R.style.Theme_MoviesApp)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initView()
     }
+
+    private fun initView() {
+        // Reference to the nav controller
+        navController = findNavController(R.id.nav_host_fragment_activity_main)
+
+
+        binding.bottomNav.setupWithNavController(navController)
+        // Visibility of the bottom navigation depending on the fragment displayed with navigation component
+        setBottomNavigationVisibility()
+    }
+
+    private fun setBottomNavigationVisibility() {
+        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+            when (destination.id) {
+                // For the Movie Details Fragment we dont need the Bottom Navigation
+                R.id.movieDetailsFragment -> {
+                    binding.bottomNav.visibility = View.GONE
+                }
+                // For any other Fragment we need the Bottom Navigation
+                else -> {
+                    binding.bottomNav.visibility = View.VISIBLE
+                }
+            }
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
+    }
+
 }
